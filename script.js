c8762c5b8a948605a7298ccd3c3e30fb98b42f11
sam8e1-777 <samvelsymonian@gmail.com>
2025-10-16 19:28:47 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const toDoList = document.getElementById('toDoList');
+const toDoInput = document.getElementById('toDoInput');
+
+function save(){
+    if(!toDoInput.value){
+        return;
+    }else{
+        const toDo = document.createElement('p');
+        toDo.textContent = toDoInput.value;
+        toDoInput.value = null;
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const doneBtn = document.createElement('button');
+        doneBtn.textContent = "📌";
+        doneBtn.classList.add('doneBtn')
+
+        doneBtn.addEventListener('click', () => {
+            toDo.classList.toggle('active');
+            doneBtn.classList.toggle('active');
+            if(doneBtn.textContent != "✅"){
+                doneBtn.textContent = "✅"
+            }else{
+                doneBtn.textContent = "📌";
+            }
+        })
+
+        const delBtn = document.createElement('button')
+        delBtn.textContent = "🗑️";
+        delBtn.classList.add('delBtn')
+
+        delBtn.addEventListener('click', () => {
+            card.remove();
+        })
+
+        card.appendChild(doneBtn);
+        card.appendChild(toDo);
+        card.appendChild(delBtn);    
+        toDoList.prepend(card);        
+    }
+
+}
